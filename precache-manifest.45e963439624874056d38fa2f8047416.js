3b8eb80f85df8e6e385e689d517ffe491e313dc2
Raghu <ragshri@gmail.com>
2020-08-05 13:32:11 +0530

    Updates

---
+++ precache-manifest.45e963439624874056d38fa2f8047416.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5587f2267c8a4dd0f812e02b535bd208",
+    "url": "/token-creator/index.html"
+  },
+  {
+    "revision": "4b7a2f1d8422dc3d22c8",
+    "url": "/token-creator/static/css/2.4cd87f0b.chunk.css"
+  },
+  {
+    "revision": "3df638ae4d32844b2ea8",
+    "url": "/token-creator/static/css/main.1eadc8cf.chunk.css"
+  },
+  {
+    "revision": "4b7a2f1d8422dc3d22c8",
+    "url": "/token-creator/static/js/2.8369e72d.chunk.js"
+  },
+  {
+    "revision": "3df638ae4d32844b2ea8",
+    "url": "/token-creator/static/js/main.bc1afac7.chunk.js"
+  },
+  {
+    "revision": "90e075181f4adfac5543",
+    "url": "/token-creator/static/js/runtime-main.bf81d3f5.js"
+  },
+  {
+    "revision": "13edfe02799ec7247f75136a58459621",
+    "url": "/token-creator/static/media/banner-bg.13edfe02.png"
+  },
+  {
+    "revision": "7ba262cf61a099208ca1e4abce36b3c1",
+    "url": "/token-creator/static/media/logo.7ba262cf.png"
+  }
+]);
